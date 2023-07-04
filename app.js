d0a69a55fcb6ca53a5a8390d5c009978303603c7
Alexis Indick <60584601+aindick@users.noreply.github.com>
2023-07-04 12:20:09 -0400

    Fixed hbs files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const hbs = require('hbs');
+var hbs = require('hbs');
 
 
 var indexRouter = require('./app_server/routes/index');
@@ -16,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
+hbs.registerPartials(path.join(__dirname, 'app_server', '/views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
